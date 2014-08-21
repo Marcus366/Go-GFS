c060df75c31b641a94c9f703ba08cf886e37a5d3
sysu2012zzp <marcus.zeng1224@gmail.com>
2014-08-21 22:59:28 +0800

    add flag parser

--- apps/chunk/chunk.go
+++ apps/chunk/chunk.go
@@ -3,13 +3,25 @@ package main
 import (
   "fmt"
   "net"
+  "flag"
 
   "github.com/sysu2012zzp/Go-GFS/chunk"
 )
 
+var (
+  MasterIP = flag.String("master-ip-address", "127.0.0.1", "IP Address of Master Server")
+)
+
 func main() {
+  flag.Parse()
+
+  ip := net.ParseIP(*MasterIP)
+  if ip == nil {
+    fmt.Println("Invalid Master IP Address:", *MasterIP)
+    return
+  }
+
   exitChan := make(chan string)
-  ip := net.ParseIP("127.0.0.1")
   c := chunk.NewChunk(ip)
   c.Main(exitChan)
   <-exitChan
