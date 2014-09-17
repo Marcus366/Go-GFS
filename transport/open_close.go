6fb70799bd18fbd62fcd7079dfc80416614c46ec
sysu2012zzp <marcus.zeng1224@gmail.com>
2014-09-17 22:35:01 +0800

    add Open flag and mode

--- transport/open_close.go
+++ transport/open_close.go
@@ -1,11 +1,31 @@
 package transport
 
 import (
-  "os"
   "errors"
 )
 
-type FileMode os.FileMode
+const (
+    O_RDONLY = 0x0
+    O_WRONLY = 0x1
+    O_RDWR   = 0x2
+    O_APPEND = 0x400
+    O_CREATE = 0x40
+
+    /* the following flag maybe supported later
+     * O_EXCL
+     * O_SYNC
+     * O_TRUNC
+    */
+)
+
+type FileMode uint32
+const (
+    ModeDir        FileMode = 1 << (32 - 1 - iota)
+    ModeAppend
+    ModeExlusive
+    ModeTemporary
+    ModeSymlink
+)
 
 type ManagerCallback interface {
   OpenFile(name string, flag int, perm FileMode) (int32, error)
